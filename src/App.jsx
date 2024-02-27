c3462e8ecebc31e8307da7a39d2fc9e6668b5188
Siddhant Singh <siddhant3646@gmail.com>
2024-02-28 00:14:45 +0530

    Hillo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export default function App()
+{
+    return(
+        <div>
+            <h1>Learning about CodeEditor,NodeJS and NPM</h1>
+            <ul>
+            <li>For Editor use - VS Code</li>
+            <li>Install Node</li>
+            <li>Install NPM (Installed along with node.)</li>
+            <li>Next we will learn Prop.</li>
+            </ul>
+        </div>
+
+    );
+}
